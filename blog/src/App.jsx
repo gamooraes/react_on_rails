c17b430fbb6fc09ee44e4d400f64063e40e2b59c
Gabriel Moraes <gariel.m.oliveira95@gmail.com>
2025-04-26 20:28:37 -0300

    Adicionado o Vit React blog app + componente PostsList

---
+++ blog/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import PostsList from './features/posts/PostsList'
+
+function App() {
+
+  return <>
+    <div className='app'>
+      <h1>Reac on Rails Blog</h1>
+      <p>Fin this application on blog/src/App.jsx</p>
+      <PostsList />
+
+    </div>
+  </>;
+}
+
+export default App
