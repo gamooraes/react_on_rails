a039c760ecad913471b6c37d2e8be87fbfc4cc7d
Gabriel Moraes <gariel.m.oliveira95@gmail.com>
2025-04-27 14:10:46 -0300

    Developed delete function

--- blog/src/features/posts/PostsList.jsx
+++ blog/src/features/posts/PostsList.jsx
@@ -1,5 +1,6 @@
 //API URL comes from the .env.development file
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { API_URL } from "../../constants";
 
@@ -8,6 +9,7 @@ function PostsList() {
     const [posts, setPosts] = useState([]);
     const [, setLoeading] = useState(true);
     const [, setError] = useState(null);
+    const navigate = useNavigate();
     // Fetch posts from API
     useEffect(() => {
         async function loadPosts(params) {
@@ -29,7 +31,28 @@ function PostsList() {
         }
         loadPosts();
     }, [])
-
+    // Delete post
+    async function deletePost(id) {
+        setLoeading(true);
+        setError(null);
+        try {
+            const response = await fetch(`${API_URL}/${id}`, {
+                method: "DELETE",
+            });
+            if (response.ok) {
+                setPosts(posts.filter((post) => post.id !== id));
+                navigate(`/`);
+            } else {
+                throw response;
+            }
+        } catch (e) {
+            setError("An error occurred. Awkward...");
+            console.log("Error: ", e);
+        } finally {
+            setLoeading(false);
+        }
+    }
+    if (!posts) return null;
     return <div>
         {posts.map((post) => (
             <div key={post.id} className="post-container">
@@ -38,8 +61,19 @@ function PostsList() {
                         {post.title}
                     </Link>
                 </h2>
-                <p>{post.body}</p>
-
+                <div className="post-links">
+                    <Link to={`/posts/${post.id}`} className="post-link">
+                        View
+                    </Link>
+                    {" | "}
+                    <Link to={`/posts/edit/${post.id}`} className="post-link">
+                        Edit
+                    </Link>
+                    {" | "}
+                    <button onClick={() => deletePost(post.id)} className="post-link">
+                        Delete
+                    </button>
+                </div>
             </div>
         ))}
     </div>
