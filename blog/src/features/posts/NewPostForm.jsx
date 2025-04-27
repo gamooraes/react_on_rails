a81594eb1b3ab30852cb7d0cfe1448e7730e7cc5
Gabriel Moraes <gariel.m.oliveira95@gmail.com>
2025-04-26 23:32:06 -0300

    Added NewPost component and make a change in the api/post_controller to resume locale to api_v1_post

---
+++ blog/src/features/posts/NewPostForm.jsx
@@ -0,0 +1,72 @@
+//Create a component for new posts in the app
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../../constants";
+
+function NewPostForm() {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    // Handle form submission
+    async function handleSubmit(event) {
+        event.preventDefault();
+        setLoading(true);
+        setError(null);
+        const postData = { title, body };
+        try {
+            const response = await fetch(API_URL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(postData),
+            });
+
+            if (response.ok) {
+                const { id } = await response.json();
+                navigate(`/posts/${id}`);
+            } else {
+                throw response;
+            }
+        } catch (e) {
+            setError("An error occurred. Awkward...");
+            console.log("Error: ", e);
+        } finally {
+            setLoading(false);
+        }
+    }
+    return (
+        <div className="new-post-form">
+            <h2>Create a New Post</h2>
+            {error && <p className="error">{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="body">Body:</label>
+                    <textarea
+                        id="body"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                        required
+                    ></textarea>
+                </div>
+                <button type="submit" disabled={loading}>
+                    {loading ? "Creating..." : "Create Post"}
+                </button>
+            </form>
+        </div>
+    );
+}
+export default NewPostForm;
