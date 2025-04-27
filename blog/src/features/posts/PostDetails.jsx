b2dfaaf9220fa6230e47bb92f4b6bc8996327603
Gabriel Moraes <gariel.m.oliveira95@gmail.com>
2025-04-27 17:03:21 -0300

    refactoring components PostList, PostDetails and the services

--- blog/src/features/posts/PostDetails.jsx
+++ blog/src/features/posts/PostDetails.jsx
@@ -1,46 +1,58 @@
 //Create the component PostDetails to show the details of a post
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
-import { API_URL } from "../../constants";
-
+import { useNavigate } from "react-router-dom";
+import { deletePost as deletePostService, fetchPost } from "../../services/postService";
 
 function PostDetails() {
     const { id } = useParams();
     const [post, setPost] = useState(null);
     const [, setLoading] = useState(true);
     const [, setError] = useState(null);
-
+    const navigate = useNavigate();
+    console.log("PostDetails: ", id);
     // Fetch post from API
     useEffect(() => {
         async function loadPost() {
             try {
-                const response = await fetch(`${API_URL}/${id}`);
-                if (response.ok) {
-                    const json = await response.json();
-                    setPost(json);
-                    console.log(json);
-                } else {
-                    throw response;
-                }
+                const json = await fetchPost(id);
+                setPost(json);
+                setLoading(false);
             } catch (e) {
                 setError("An error occurred. Awkward...");
                 console.log("Error: ", e);
-            } finally {
-                setLoading(false);
             }
         }
         loadPost();
     }, [id]);
 
+    const deletePost = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            await deletePostService(id);
+            setPost(posts.filter((post) => post.id !== id));
+            navigate("/");
+        } catch (e) {
+            setError("An error occurred. Awkward...");
+            console.log("Error: ", e);
+        } finally {
+            setLoading(false);
+        }
+    }
     if (!post) return null;
 
     return (
         <div className="post-details">
             <h2>{post.title}</h2>
             <p>{post.body}</p>
-            <Link to="/posts">Back to Posts List</Link>
+            <Link to="/">Back to Posts List</Link>
             {" | "}
             <Link to={`/posts/edit/${post.id}`}>Edit Post</Link>
+            {" | "}
+            <button onClick={() => deletePost(post.id)} className="post-link">
+                Delete
+            </button>
         </div>
     );
 }
